a55b9e6a658e793140a8e21117db651e72583be5
nandoerni <erninan@students.zhaw.ch>
2021-10-26 17:30:25 +0200

    aufgabe 2

--- Woche5_test_zur_kombinatorik.py
+++ Woche5_test_zur_kombinatorik.py
@@ -1,4 +1,6 @@
-from itertools import chain, combinations
+import math
+
+import numpy as np
 
 
 def potenz_menge(fullset):
@@ -40,6 +42,26 @@ def potenz_menge_anzahl_elemente(elemente):
     return 2 ** elemente
 
 
+def auf_wie_viele_arten_kann_man_die_Folge_anordnen(folge):
+    n = len(folge)
+
+    dic = {}
+    for i in range(n):
+        key = folge[i]
+        if key in dic:
+            dic[key] += 1
+        else:
+            dic[key] = 1
+
+    k = 1
+    for x in dic:
+        k *= math.factorial(dic[x])
+    return math.factorial(n) / k
+
+
+
+# Beispiele:
 print_potenz_menge(['1', '10', 'a', 't'])
 print(potenz_menge_anzahl_elemente(22))
 
+print(auf_wie_viele_arten_kann_man_die_Folge_anordnen([-2, 3, 2, 3, 0, 0]))
