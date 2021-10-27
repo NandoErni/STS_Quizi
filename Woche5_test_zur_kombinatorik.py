60f9af1cec4fb06820864dee83e857a334d9b7fc
nandoerni <erninan@students.zhaw.ch>
2021-10-27 21:03:51 +0200

    yesyes

--- Woche5_test_zur_kombinatorik.py
+++ Woche5_test_zur_kombinatorik.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy as np
 
 
@@ -59,9 +58,35 @@ def auf_wie_viele_arten_kann_man_die_Folge_anordnen(folge):
     return math.factorial(n) / k
 
 
+def computer_program(input_vektor, prozent_der_testlaeufe):
+    comb = 1
+
+    for x in input_vektor:
+        comb *= len(x)
+
+    return math.ceil(comb * prozent_der_testlaeufe)
+
+
+def sicherheitskopie1(anzahlKopien, anzahlServer):
+    n = anzahlKopien + anzahlServer - 1
+    k = anzahlKopien
+    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+
+def sicherheitskopie2(anzahlKopien, anzahlServer):
+    anzahlKopien -= anzahlServer
+    return sicherheitskopie1(anzahlKopien, anzahlServer)
+
+
+def zahlen_mit_ziffer(stellen):
+    return 10 ** stellen - 9 ** stellen
+
 
 # Beispiele:
 print_potenz_menge(['1', '10', 'a', 't'])
 print(potenz_menge_anzahl_elemente(22))
-
 print(auf_wie_viele_arten_kann_man_die_Folge_anordnen([-2, 3, 2, 3, 0, 0]))
+print(computer_program([np.arange(-1, 3, 1), np.arange(-3, 5, 1), np.arange(-2, 6, 1), np.arange(0, 3, 1)], 0.8))
+print(sicherheitskopie1(10, 10))
+print(sicherheitskopie2(11, 10))
+print(zahlen_mit_ziffer(7))
